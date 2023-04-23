4d0e559417198dc8d59bedc9192480bb84f052ef
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2023-04-23 21:38:30 +0530

    stack works, parser works, impl-ed basic opcodes

--- bytecode-parser.ts
+++ bytecode-parser.ts
@@ -4,20 +4,40 @@ const bytecode = process.argv.slice(2,)[0].slice(2,); // the first slice gets us
 if (!bytecode) throw new Error("No bytecode given.");
 
 
-// 60 01, 60 02, 01
+const stack: bigint[] = [];
 
 for (let counter = 0; counter < bytecode.length; ) {
-  const opcode = bytecode.slice(counter, counter + 2);
+  console.log("Stack:", stack, "\n");
+  const opcode = parseInt("0x" + bytecode.slice(counter, counter + 2)) as keyof typeof instructions;
+  if (!opcode) throw new Error(`Invalid OPCODE: ${opcode}`);
   counter += 2;
-  
-  if (opcode.slice(0, 4) == "PUSH") {
-    const argumentsFromBytecodeLength = parseInt(opcode.slice(4,)); // read these number of bytes directly off the bytecode
-    const input = bytecode.slice(counter, counter+argumentsFromBytecodeLength*2); // *2 as each byte = 2 characters in hex
-    // append to stack
+
+  if (opcode >= 0x5F && opcode <= 0x7F) { // PUSH range
+    const numberOfArgs = parseInt(instructions[opcode].name.slice(4,)); // we get the number of bytes, to be read, from name; eg: PUSH1 => 1 byte
+    const pushArgs = BigInt(bytecode.slice(counter, counter+numberOfArgs*2)); // *2 as each byte = 2 characters in hex
+    counter += numberOfArgs*2; // *2 cuz above
+
+    console.log("0x" + opcode, instructions[opcode].name, pushArgs);
+    if (stack.length == 1024) throw new Error("Maximum call stack exceeded.");
+
+    stack.push(pushArgs);
+    continue;
   }
 
   const stackInputLength = instructions[opcode].implementation.length; // read these many words from stack
-  const stackInput = bytecode.slice(counter, stackInputLength); // stackInputLength*2 as each arg would be one byte => 2 characters in hex
+  const stackInput: bigint[] = [];
+  for (let inputNum = 0; inputNum < stackInputLength; inputNum++) {
+    const input = stack.pop();
+    if (!input) throw new Error("Call stack underflow");
 
-  // perform operation with args
+    stackInput.push(input);
+  }
+
+  counter += stackInputLength;
+  console.log("0x" + opcode, instructions[opcode].name, stackInput);
+
+  const result = instructions[opcode].implementation(...(stackInput as [bigint, bigint]));
+  stack.push(...result);
 }
+
+console.log("Final stack:", stack);
--- opcodes.ts
+++ opcodes.ts
@@ -4,44 +4,53 @@
   * The math needs to be fixed to replicate EVM errors / edgecases.
   **/
 type Instruction = {
-  opcode: string,
   name: string,
   minimumGas: number,
   implementation: Function
 }
 
-export const instructions: { [opcode: string]: Instruction }  = {
-  '00': {
-    opcode: '00',
+export const instructions = {
+  0x00: {
     name: 'STOP',
     minimumGas: 0,
     implementation: () => { throw new Error("STOP") }
   },
-  '01': {
-    opcode: '01',
+  0x01: {
     name: 'ADD',
     minimumGas: 3,
     implementation: (a: bigint, b: bigint) => [a + b],
   },
-  '02': {
-    opcode: '02',
+  0x02: {
     name: 'MUL',
     minimumGas: 5,
     implementation: (a: bigint, b: bigint) => [a * b],
   },
-  '03': {
-    opcode: '03',
+  0x03: {
     name: 'SUB',
     minimumGas: 3,
     implementation: (a: bigint, b: bigint) => [a - b],
   },
-  '04': {
-    opcode: '04',
+  0x04: {
     name: 'DIV',
     minimumGas: 5,
     implementation: (a: bigint, b: bigint) => {
-      if (b == 0n || a == 0n) return 0;
+      if (b == 0n || a == 0n) return [0n];
       return [(a / b)];
     },
+  },
+  0x5F: {
+    name: 'PUSH0',
+    minimumGas: 2,
+    implementation: () => [0n],
+  },
+  0x60: {
+    name: 'PUSH1',
+    minimumGas: 3,
+    implementation: (a: bigint) => [a],
+  },
+  0x61: {
+    name: 'PUSH2',
+    minimumGas: 3,
+    implementation: (a: bigint, b: bigint) => [a, b],
   },
 };
