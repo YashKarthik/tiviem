0bbcfe44e8fabe3612516edbca53fc676ef1c9b8
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2023-04-25 18:29:47 +0530

    added tests github.com/w1nt3r-eth/evm-from-scratch

---
+++ evm.test.ts
@@ -0,0 +1,16 @@
+import { evm } from "./bytecode-parser";
+import { expect, test, describe } from "bun:test";
+//@ts-expect-error The file exists!
+import tests from "./tests";
+
+for (const t of tests as any) {
+  test(t.name, () => {
+    // Note: as the test cases get more complex, you'll need to modify this
+    // to pass down more arguments to the evm function (e.g. block, state, etc.)
+    // and return more data (e.g. state, logs, etc.)
+    const result = evm(t.code.bin);
+
+    expect(result.success).toEqual(t.expect.success);
+    expect(result.stack).toEqual(t.expect.stack.map((item:any) => BigInt(item)));
+  });
+}
